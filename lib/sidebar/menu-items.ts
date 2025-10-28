5eb9c730d25d6a528b8d9731a20f4ed26e14a64f
Masaya <masaya.nishimura0927@gmail.com>
2025-10-28 21:04:44 +0900

    add: sidebar

---
+++ lib/sidebar/menu-items.ts
@@ -0,0 +1,20 @@
+import { Home, NotebookPen, Settings } from "lucide-react";
+
+// Menu items.
+export const items = [
+  {
+    title: "ホーム",
+    url: "/dashboard",
+    icon: Home,
+  },
+  {
+    title: "日記",
+    url: "/dashboard/diary",
+    icon: NotebookPen,
+  },
+  {
+    title: "Settings",
+    url: "/dashboard/settings",
+    icon: Settings,
+  },
+];
