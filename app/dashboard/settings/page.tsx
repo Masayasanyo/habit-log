e192882616a5b0fb434ad3b1cdf18e2d84abaaed
Masaya <masaya.nishimura0927@gmail.com>
2025-10-29 16:06:45 +0900

    next-authを追加

---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "設定",
+};
+
+export default function Page() {
+  return (
+    <div className="">
+      <p>設定ページ</p>
+    </div>
+  );
+}
