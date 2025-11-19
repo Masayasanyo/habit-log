5a6ffb1507e3e7dd37ef3394a23366c60e30e3be
Masaya <masaya.nishimura0927@gmail.com>
2025-11-19 19:47:31 +0900

    Add: DiaryArchiveTable.tsx

--- components/containers/diary-archive/DiaryArchive.tsx
+++ components/containers/diary-archive/DiaryArchive.tsx
@@ -3,21 +3,13 @@
 
 import { fetchDiaries } from "@/actions/diaries-actions";
 import { columns } from "@/components/containers/diary-archive/columns";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import DiaryArchiveSearchDialog from "@/components/containers/diary-archive/DiaryArchiveSearchDialog";
+import DiaryArchiveTable from "@/components/containers/diary-archive/DiaryArchiveTable";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { getDate, getDateOneMonthAgo } from "@/lib/date/date";
 import { Diary } from "@/types/diaries";
 import {
   ColumnFiltersState,
-  flexRender,
   getCoreRowModel,
   getFilteredRowModel,
   getPaginationRowModel,
@@ -28,7 +20,6 @@ import {
 } from "@tanstack/react-table";
 import * as React from "react";
 import { useEffect, useState } from "react";
-import DiaryArchiveSearchDialog from "./DiaryArchiveSearchDialog";
 
 export function DiaryArchive() {
   const [sorting, setSorting] = React.useState<SortingState>([]);
@@ -80,69 +71,8 @@ export function DiaryArchive() {
         <DiaryArchiveSearchDialog setDiaries={setDiaries} table={table} />
       </CardHeader>
       <CardContent className="h-full">
-        <div className="w-full">
-          <div className="overflow-hidden rounded-md border">
-            <Table>
-              <TableHeader>
-                {table.getHeaderGroups().map((headerGroup) => (
-                  <TableRow key={headerGroup.id}>
-                    {headerGroup.headers.map((header) => {
-                      return (
-                        <TableHead key={header.id}>
-                          {header.isPlaceholder
-                            ? null
-                            : flexRender(header.column.columnDef.header, header.getContext())}
-                        </TableHead>
-                      );
-                    })}
-                  </TableRow>
-                ))}
-              </TableHeader>
-              <TableBody>
-                {table.getRowModel().rows?.length ? (
-                  table.getRowModel().rows.map((row) => (
-                    <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
-                      {row.getVisibleCells().map((cell) => (
-                        <TableCell key={cell.id}>
-                          {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                        </TableCell>
-                      ))}
-                    </TableRow>
-                  ))
-                ) : (
-                  <TableRow>
-                    <TableCell colSpan={columns.length} className="h-24 text-center">
-                      日記がありません。
-                    </TableCell>
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </div>
-        </div>
-
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <div className="space-x-2">
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={() => table.previousPage()}
-              disabled={!table.getCanPreviousPage()}
-            >
-              前へ
-            </Button>
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={() => table.nextPage()}
-              disabled={!table.getCanNextPage()}
-            >
-              次へ
-            </Button>
-          </div>
-        </div>
+        <DiaryArchiveTable table={table} />
       </CardContent>
-      <CardFooter className="flex-col gap-2"></CardFooter>
     </Card>
   );
 }
---
+++ components/containers/diary-archive/DiaryArchiveTable.tsx
@@ -0,0 +1,81 @@
+import { columns } from "@/components/containers/diary-archive/columns";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { DiaryColumns } from "@/types/diaries";
+import type { Table as TableType } from "@tanstack/react-table";
+import { flexRender } from "@tanstack/react-table";
+
+export default function DiaryArchiveTable({ table }: { table: TableType<DiaryColumns> }) {
+  return (
+    <div>
+      <div className="w-full">
+        <div className="overflow-hidden rounded-md border">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(header.column.columnDef.header, header.getContext())}
+                      </TableHead>
+                    );
+                  })}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={columns.length} className="h-24 text-center">
+                    日記がありません。
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            前へ
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            次へ
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
