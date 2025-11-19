25f0e3b9da5b38384f42bb9335c0330cf90c4201
Masaya <masaya.nishimura0927@gmail.com>
2025-11-19 09:41:07 +0900

    Fix: main root loading color

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,11 +1,9 @@
-// TODO: change text color or bg color
-
 import { Spinner } from "@/components/ui/spinner";
 
 export default function Loading() {
   return (
-    <div className="mt-30 flex w-full items-center justify-center gap-4">
-      <Spinner className="size-8 text-primary" />
+    <div className="mt-50 flex w-full text-secondary items-center justify-center gap-4">
+      <Spinner className="size-8" />
       <span className="text-xl">読み込み中...</span>
     </div>
   );
